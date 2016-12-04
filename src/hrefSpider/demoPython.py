fe4b105fd8a66ebad654a8f3f64ae5b2b25aae86
fandong <871119239@qq.com>
2016-12-04 16:49:55 +0800

    遇到爬的网页为空，就会停止

--- src/hrefSpider/demoPython.py
+++ src/hrefSpider/demoPython.py
@@ -3,7 +3,13 @@ Created on 2016年12月3日
 
 @author: fandong
 '''
+from urllib.request import urlopen
+from urllib.parse import urlparse
 def printHello():
     print('hello!')
     
 printHello()
+html=urlopen("http://www.cnblogs.com")
+print(html.read())
+print(urlparse("http://www.cnblogs.com").netloc)
+print(urlparse(urlparse("http://www.cnblogs.com").netloc).netloc+";;;")
--- src/hrefSpider/spuerLink.py
+++ src/hrefSpider/spuerLink.py
@@ -20,11 +20,11 @@ def getInternalLinks(bsObj,includeUrl):
     internalLinks=[]
     #找到所有以‘／’开头的连接
     try:
-        for link in bsObj.findAll("a",href=re.compile("^(/|.*"+includeUrl+")")):
+        for link in bsObj.findAll("a",href=re.compile("^("+includeUrl+"|/)[/].*$")):
             if link.attrs["href"] is not None:
                 if link.attrs["href"] not in internalLinks:
-                    if(link.attrs["href"].startswitch("/")):
-                        internalLinks.append(includeUrl+link.attr["href"])
+                    if(link.attrs["href"].startswith("/")):
+                        internalLinks.append(includeUrl+link.attrs["href"])
                     else:
                         internalLinks.append(link.attrs["href"])
         if len(internalLinks)==0:
@@ -49,9 +49,9 @@ def getExternalLinks(bsObj,excludeUrl):
 def getRandomExternalLink(startingPage):
     try:
         html=urlopen(startingPage)
-        print(html.read())
         bsObj=BeautifulSoup(html)
-        print(urlparse(startingPage).netloc)
+        for namelist in getExternalLinks(bsObj,urlparse(startingPage).netloc):
+            print(namelist)
         externalLinks=getExternalLinks(bsObj,urlparse(startingPage).netloc)
         if len(externalLinks)==0:
             print("没有外部链接，查看网站")
@@ -62,23 +62,34 @@ def getRandomExternalLink(startingPage):
             if len(internalLinks)>0:    
                 return getRandomExternalLink(internalLinks[random.randint(0,len(internalLinks)-1)])
             else:
-                return startingPage
+                return internalLinks.append("http://www.baidu.com")
         else:
             return externalLinks[random.randint(0,len(externalLinks)-1)]
     except HTTPError as e:
         print("http 异常")
     
+    
 #开始调用方法
 def followExternalOnly(startingSite):
-    
-    externalLink=getRandomExternalLink(startingSite)
-    print("Random external link is :"+externalLink)
-    followExternalOnly(externalLink)
+    try:
+        externalLink=getRandomExternalLink(startingSite)
+        print("Random external link is :"+externalLink)
+        followExternalOnly(externalLink)
+    except SystemError as e:
+        print(" 123")
     
 
 followExternalOnly("http://www.cnblogs.com")
+#调用内部链接查询
+#html=urlopen("http://www.cnblogs.com")
+#bsObj=BeautifulSoup(html)
+#for name in getInternalLinks(bsObj,"http://www.cnblogs.com"):
+#    print(name)
+#for link in bsObj.findAll("a",href=re.compile("^http://www.cnblogs.com/.*$")):
+ #   print(link.attrs["href"])
         
-    
+#for name in getExternalLinks(bsObj,urlparse("http://www.cnblogs.com").netloc):
+#    print(name)
     
         
                               
